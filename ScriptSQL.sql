fa93c1ad471c224c78c37ac3830906a98222d05a
gcwinter <gabrielcwinter@gmail.com>
2024-04-22 23:12:45 -0300

    Entrega02(bd): script do banco de dados

---
+++ ScriptSQL.sql
@@ -0,0 +1,9 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  Winter
+ * Created: 22 de abr. de 2024
+ */
+
---
+++ src/main/java/br/com/gabrielwinterusjt/mavenproject1/ScriptSQL.sql
@@ -0,0 +1,35 @@
+use defaultdb ;
+
+CREATE TABLE social_events(
+  cod_usuario INT PRIMARY KEY NOT NULL AUTO_INCREMENT ,
+  login VARCHAR(200) NOT NULL,
+  password VARCHAR(200)NOT NULL,
+  name VARCHAR(200) ,
+  gender VARCHAR(200) ,
+  email VARCHAR(200) ,
+  cpf VARCHAR(11) ,
+  age INT 
+);
+
+drop table social_events;
+
+
+SELECT * FROM social_events;
+
+
+INSERT INTO social_events
+(login, password) VALUES
+('TEST', 'TEST');
+
+SELECT  *
+FROM social_events;
+
+UPDATE social_events SET
+login='admin',password='admin'
+WHERE cod_usuario = 1;
+
+DELETE FROM social_events
+WHERE cod_usuario = 2;
+
+UPDATE social_events SET
+password='' WHERE cod_usuario = 1;
